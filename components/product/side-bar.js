c2bb78251ac3fa602ba90936ac6bf2b22729fe05
dotec-korea <abhi.dotec@gmail.com>
2023-07-19 19:57:28 +0530

    Updated sorting on product page

--- components/product/side-bar.js
+++ components/product/side-bar.js
@@ -17,41 +17,46 @@ const SideBar = ({
   return (
     products && (
       <>
-        {products.map((product, i) => {
-          return (
-            <div key={i} className='mb-6'>
-              {product && (
-                <h5
-                  className={`font-bold uppercase tracking-wide text-sm mb-3 cursor-pointer ${
-                    kebab(product?.title) === range && 'text-blue-700'
-                  }`}
-                  onClick={() => setRangeAndProduct(product?.title, '')}
-                >
-                  {product?.id && String.fromCharCode(product?.id + 64) + '. '}
-                  {product.title}
-                </h5>
-              )}
-              {product.productCollection.items.map((item, j) => {
-                return (
-                  item && (
-                    <p
-                      key={j}
-                      className={`text-sm my-1 tracking-wide cursor-pointer hover:text-blue-700 ${
-                        item.sys.id === productId ? 'text-blue-700' : ''
-                      }`}
-                      onClick={() =>
-                        setRangeAndProduct(product.title, item.sys.id)
-                      }
-                    >
-                      {item?.id && item?.id + '. '}
-                      {item?.title}
-                    </p>
-                  )
-                );
-              })}
-            </div>
-          );
-        })}
+        {products
+          .sort((x, y) => x.id - y.id)
+          .map((product) => {
+            return (
+              <div key={product.id} className='mb-6'>
+                {product && (
+                  <h5
+                    className={`font-bold uppercase tracking-wide text-sm mb-3 cursor-pointer ${
+                      kebab(product?.title) === range && 'text-blue-700'
+                    }`}
+                    onClick={() => setRangeAndProduct(product?.title, '')}
+                  >
+                    {product?.id &&
+                      String.fromCharCode(product?.id + 64) + '. '}
+                    {product.title}
+                  </h5>
+                )}
+                {product.productCollection.items
+                  .sort((x, y) => x.id - y.id)
+                  .map((item) => {
+                    return (
+                      item && (
+                        <p
+                          key={item.id}
+                          className={`text-sm my-1 tracking-wide cursor-pointer hover:text-blue-700 ${
+                            item.sys.id === productId ? 'text-blue-700' : ''
+                          }`}
+                          onClick={() =>
+                            setRangeAndProduct(product.title, item.sys.id)
+                          }
+                        >
+                          {item?.id && item?.id + '. '}
+                          {item?.title}
+                        </p>
+                      )
+                    );
+                  })}
+              </div>
+            );
+          })}
       </>
     )
   );
